272cfb8583a1e4b6d5e8e912bd759d2ebebd8da7
Philipp Pallentin <Philipp.pallentin@web.de>
2020-10-11 21:08:08 +0200

    added travis build scripts

---
+++ .travis/install-flatpak.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ex
+sudo apt-get update
+
+sudo apt install -y flatpak
+sudo apt install -y flatpak-builder
+
+flatpak install -y flathub org.kde.Sdk//5.11
+flatpak install -y flathub org.kde.Platform//5.11
