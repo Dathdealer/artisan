d0affa8974808abce4141884703e10fcced0b135
Philipp Pallentin <Philipp.pallentin@web.de>
2020-10-11 21:35:58 +0200

    fix travis install

--- .travis/install-flatpak.sh
+++ .travis/install-flatpak.sh
@@ -6,5 +6,5 @@ sudo apt-get update
 sudo apt install -y flatpak
 sudo apt install -y flatpak-builder
 
-flatpak install -y flathub org.kde.Sdk//5.11
-flatpak install -y flathub org.kde.Platform//5.11
+flatpak install -y --user flathub org.kde.Sdk//5.11
+flatpak install -y --user flathub org.kde.Platform//5.11
