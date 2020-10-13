74923b440337f531893593eae14f30742eddf10b
Philipp Pallentin <Philipp.pallentin@web.de>
2020-10-13 17:32:47 +0200

    test travis

--- .travis/install-flatpak.sh
+++ .travis/install-flatpak.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-set -e
+set -ex
 sudo apt-get update
-
 sudo apt install -y flatpak
 sudo apt install -y flatpak-builder
 
-flatpak install -y --user flathub org.kde.Sdk//5.11
-flatpak install -y --user flathub org.kde.Platform//5.11
+#flatpak install -y --user flathub org.kde.Sdk//5.11
+#flatpak install -y --user flathub org.kde.Platform//5.11
