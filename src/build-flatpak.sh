20cc9722b51357b58cd4fc1f7ecc77fb824f099b
Philipp Pallentin <Philipp.pallentin@web.de>
2020-10-13 17:47:48 +0200

    pack flatpak to src

--- src/build-flatpak.sh
+++ src/build-flatpak.sh
@@ -1,3 +1,3 @@
-flatpak-builder --repo=../repo ../build-dir org.flatpak.artisan.yaml --force-clean
-cd ..
+flatpak-builder --repo=repo build-dir org.flatpak.artisan.yaml --force-clean
+cd
 flatpak build-bundle repo artisan.flatpak org.flatpak.artisan --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
