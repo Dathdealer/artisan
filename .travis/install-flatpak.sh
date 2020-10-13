938cf1659d13bccb36c795c0549768fdb4ae7d64
Philipp Pallentin <Philipp.pallentin@web.de>
2020-10-13 20:39:13 +0200

    link

--- .travis/install-flatpak.sh
+++ .travis/install-flatpak.sh
@@ -6,6 +6,6 @@ sudo apt-get update
 sudo apt-get install -y flatpak
 sudo apt-get install -y flatpak-builder
 
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub org.kde.Sdk//5.11
 flatpak install -y  flathub org.kde.Platform//5.11
