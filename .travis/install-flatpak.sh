7509fd7aad0add613dac6eb82108899dbf9c2bee
Philipp Pallentin <Philipp.pallentin@web.de>
2020-10-13 17:48:54 +0200

    fixed apt-get update

--- .travis/install-flatpak.sh
+++ .travis/install-flatpak.sh
@@ -2,8 +2,8 @@
 
 set -ex
 sudo apt-get update
-sudo apt install -y flatpak
-sudo apt install -y flatpak-builder
+sudo apt-get install -y flatpak
+sudo apt-get install -y flatpak-builder
 
 #flatpak install -y --user flathub org.kde.Sdk//5.11
 #flatpak install -y --user flathub org.kde.Platform//5.11
